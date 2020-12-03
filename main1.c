5786ea7f6838e771c521ba1b88af39f8b8dd9799
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-12-03 16:04:40 +0900

    03

--- main1.c
+++ main1.c
@@ -4,29 +4,33 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 struct Book {
 	int number;
-	char title[20];
+	char title[10];
 };
 
 int main(int argc, char *argv[]) {
 	
-	struct Book *p;
+	int i;
+	struct Book **bookshelf;
 	
-	p = ( struct Book* )malloc(2*sizeof(struct Book));
-	
-	if (p == NULL)
+	bookshelf = (struct Book**)malloc(3*sizeof(struct Book*));
+	for (i=0; i<3; i++)
 	{
-		printf("메모리 할당 오류\n");
-		return;
+		bookshelf[i] =(struct Book*)malloc(10*sizeof(struct Book));
 	}
-	p ->number = 1;
-	strcpy(p->title,"C Programming" );
 	
-	(p+1) ->number = 2;
-	strcpy((p+1)->title,"Electronics");
+	bookshelf[1][3].number = 5;
+	strcpy(bookshelf[1][3].title, "C++ Programming");
 	
-	printf(" %s %s \n",p->title, (p+1)->title );
+	(bookshelf[2]+4)->number = 3;
+	strcpy((bookshelf[2]+4)->title, "Communications Theory");
 	
-	free(p);
+	printf("book(1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+	printf("book(2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);  
+	
+	for (i=0; i<3; i++)
+		free(bookshelf[i]);
+	free(bookshelf);
+
 	
 	return 0;
 }
